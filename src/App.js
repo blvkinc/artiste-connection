dc57e5b4ac4efda1b4cb1c6dc80682e393dad89f
Dhanuja Siriwardena <dhanujasiriwardena@gmail.com>
2025-08-08 23:49:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background-container">
+        {/* Spline design iframe as background */}
+        <iframe
+          src="https://my.spline.design/untitled-6217a740e713536b7272ab5ddfaf3442/?storefront=envato-elements"
+          title="Spline Design Background"
+          className="spline-background"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay; fullscreen; camera; microphone; geolocation"
+          sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-modals"
+        />
+        
+        {/* Logo overlay */}
+        <div className="text-overlay">
+          <img src="/logo.svg" alt="Artiste Connection Logo" className="main-logo" />
+        </div>
+      </div>
     </div>
   );
 }
